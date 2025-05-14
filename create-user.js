6474efaa4c75e28e26c206b2ca2280bd362231c4
kienlepotato <168525339+kienlepotato@users.noreply.github.com>
2025-05-14 16:30:53 +1000

    added account for kien + pkg package

--- create-user.js
+++ create-user.js
@@ -2,16 +2,21 @@ const bcrypt = require('bcrypt');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./inventory.db');
 
-const username = 'admin';
-const password = 'adminpass';
-const role = 'admin';
-const email = 'palutenaisbestwaifu@gmail.com';
+// const username = 'admin';
+// const password = 'adminpass';
+// const role = 'admin';
+// const email = 'palutenaisbestwaifu@gmail.com';
 
 // const username = 'user';
 // const password = 'userpass';
 // const role = 'user';
 // const email = 'safjfvbh@gmail.com';
 
+
+const username = 'kienle';
+const password = 'Passw0rd!';
+const role = 'admin';
+const email = 'kien.leaus+shelf@gmail.com';
 
 bcrypt.hash(password, 10).then(hash => {
   db.run(`INSERT INTO users (username, password_hash, role, email) VALUES (?, ?, ?, ?)`,
